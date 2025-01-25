39b6a43d3114d37b4a5f214b56fa5e6eab6baafe
Sascha Mahmood <github@smahmood.de>
2025-01-25 17:01:39 +0100

    fixing player controls

--- bubmarine/Player/player.gd
+++ bubmarine/Player/player.gd
@@ -1,52 +1,29 @@
 extends RigidBody3D
 
-var move_speed = 2
+var move_speed = 1
 var turn_speed = 0.3
 var target_velocity = Vector3.ZERO
+var torque : int = 20
+var acc : float = 0.0
 
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	if Input.is_action_pressed("move_forward"):
-		apply_central_force(Vector3(0, 0, 2))
-	if Input.is_action_pressed("move_forward"):
-		apply_central_force(Vector3(0, 0, -2))
-	
+	var rotation_direction : Vector3 = Vector3(0, 0, 0)
+	if Input.is_action_just_pressed("horn"):
+		if !$AudioHorn.playing:
+			$AudioHorn.play()
 	if Input.is_action_pressed("move_right"):
-		apply_torque(Vector3(0, 0.2, 0))
+		rotation_direction.y -= turn_speed
 	if Input.is_action_pressed("move_left"):
-		apply_torque(Vector3(0, -0.2, 0))
-
-func _physics_process(delta):
-	
-	var direction = Vector3.ZERO
-	
+		rotation_direction.y += turn_speed
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+		acc = move_speed
+	else:
+		acc = 0
 	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-		
-	var turn_input = 0.0
-	
-	if Input.is_action_pressed("move_right"):
-		turn_input += 1
-	if Input.is_action_pressed("move_left"):
-		turn_input -= 1
-	
-	#turn_input += Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		#$Pivot.basis = Basis.looking_at(direction)
-		
-	target_velocity.x = direction.x * move_speed
-	target_velocity.z = direction.z * move_speed
-	
-	if abs(turn_input) < 0.1:
-		turn_input = 0
-		
-	rotation.y -= turn_input * turn_speed * delta
-	
-	#velocity = target_velocity.rotated(Vector3.UP, rotation.y)
-	#move_and_slide()
+		acc = -move_speed / 2
+		pass
+	state.apply_torque(rotation_direction * torque)
+	state.apply_force(global_basis.z * 100 * acc)
