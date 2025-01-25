f3bc6c0a8c839df2dd492e175cf29aeccd7bf282
rinaterina <catherinaderen@gmail.com>
2025-01-25 21:45:36 +0100

    Neigung player

--- bubmarine/Player/player.gd
+++ bubmarine/Player/player.gd
@@ -3,8 +3,9 @@ extends RigidBody3D
 var move_speed = 1
 var turn_speed = 0.3
 var target_velocity = Vector3.ZERO
-@export var torque : int = 50
+@export var torque : int = 60
 var acc : float = 0.0
+@export var swing_force = 0.8
 
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
@@ -26,6 +27,9 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	else:
 		acc = 0
 		
+	var target_tilt_z = float(rotation_direction.y) * swing_force 
+	rotation.z = lerp(rotation.z, target_tilt_z, 0.06) 
+	rotation.x = lerp(rotation.x, 0.0, 0.2)
 		
 	state.apply_torque(rotation_direction * torque)
 	state.apply_force(global_basis.z * 100 * acc)
